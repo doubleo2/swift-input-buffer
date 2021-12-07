d64d1f540baa3fd81b61f7d42c1a881a45d59f12
Enrico Stevenson-Mills <enrico@socketmobile.com>
2021-12-07 13:39:39 +0800

    Initial Commit

---
+++ inputbuffer/inputbufferApp.swift
@@ -0,0 +1,17 @@
+//
+//  inputbufferApp.swift
+//  inputbuffer
+//
+//  Created by Enrico Mills on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct inputbufferApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
